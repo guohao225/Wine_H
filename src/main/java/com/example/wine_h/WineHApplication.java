306de8b3339252874a2d6180a18681033119edb3
guohao225 <guohao225@163.com>
2021-09-29 09:31:54 +0800

    Wine project

---
+++ src/main/java/com/example/wine_h/WineHApplication.java
@@ -0,0 +1,13 @@
+package com.example.wine_h;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineHApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineHApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wine_h/WineHApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wine_h;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineHApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
